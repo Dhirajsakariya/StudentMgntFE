526df58b20396c2d4b9300366da2e655c2f77dfe
DaxaChhatrodiya <daxachhatrodiya11@gmail.com>
2024-03-15 12:02:26 +0530

    Initial-Commit

---
+++ src/Components/Login/ForgotPassword.jsx
@@ -0,0 +1,148 @@
+import React, { useState , useEffect} from 'react';
+//import './ForgotPassword.css';
+import './Login.css';
+import { useHistory } from 'react-router-dom';
+import { toast, Toaster } from 'react-hot-toast';
+import { CgMail } from 'react-icons/cg';
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
+import moment from 'moment';
+import config from './config';
+
+
+const ForgotPassword = () => {
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [showNewPassword, setShowNewPassword] = useState(false); 
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false); 
+    const navigate = useHistory();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!email || !birthday || !newPassword || !confirmPassword) {
+            toast.error('All fields are required!');
+            return;
+        }
+        const emailRegex = /^\S+@\S+\.\S+$/;
+        if (!emailRegex.test(email)) {
+            toast.error('Please enter a valid email address!');
+            return;
+        }
+        if (newPassword !== confirmPassword) {
+            toast.error("Password does not match!");
+            return;
+        }
+
+        try {
+            const response = await fetch(`${config.ApiUrl}User/ChangePassword`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: email,
+                    birthDate: birthday,
+                    password: newPassword
+                }),
+            });
+            const result = await response.text();
+            console.log(result);
+            if (result === "Password updated successfully") 
+            {      
+                toast.success("Changed  Password Successfully!");     
+                setTimeout(() => {
+                    navigate.push('/') 
+                  }, 1500); 
+                localStorage.setItem('registeredEmail', email);
+            } 
+            else if (result === "Email or BirthDate not Exist") {
+                toast.error('Email or Birthday Not Found!');
+            } 
+            else 
+            {
+                toast.error('Failed to update password!');
+            }
+    
+        } catch {
+            toast.error('Failed to update password!');
+        }
+    };
+
+    const toggleNewPasswordVisibility = () => { 
+        setShowNewPassword(!showNewPassword);
+    };
+
+    const toggleConfirmPasswordVisibility = () => { 
+        setShowConfirmPassword(!showConfirmPassword);
+    };
+
+    const customToastStyle = {
+        fontFamily: "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
+        fontSize: '16px',
+        fontWeight: 'bold',
+      };
+
+    return (
+        <div className='containerr'>
+            <form onSubmit={handleSubmit}>
+                <h2 className='signup'>Reset Password</h2>
+                <div className='form-groupl'>
+                    <label className='labell'>Email:</label>
+                    <input
+                        className='inputl'
+                        type='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder='Enter Your Email'
+                        required/> 
+                </div>
+                <CgMail className='icone'/>
+                <div className='form-groupl'>
+                    <label className='labell'>Birthdate:</label>
+                    <input className='inputl' type='date' value={birthday} max={moment().format("YYYY-MM-DD")} onChange={(e) => setBirthday(e.target.value)} required />
+                </div>
+                <div className='form-groupl'>
+                    <label className='labell'>New Password:</label>
+                    <div className='password-input'>
+                        <input 
+                            className='inputl' 
+                            type={showNewPassword ? 'text' : 'password'} 
+                            value={newPassword} 
+                            onChange={(e) => setNewPassword(e.target.value)} 
+                            pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~\@\!\#\$\%\^\&\*\?]).{8,15}$"
+                            title="Must contain at least one  number and one uppercase and one lowercase letter and One special Charecter, and at least 8 characters"
+                            placeholder='Enter New Password' 
+                            required 
+                        />
+                        {showNewPassword ? <IoEyeOutline className='iconl' onClick={toggleNewPasswordVisibility} /> : <IoEyeOffOutline className='iconl' onClick={toggleNewPasswordVisibility} />}
+                    </div>
+                
+                </div>
+                <div className='form-groupl'>
+                    <label className='labell'>Confirm Password:</label>
+                    <div className='password-input'>
+                        <input 
+                            className='inputl' 
+                            type={showConfirmPassword ? 'text' : 'password'} 
+                            value={confirmPassword} 
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~\@\!\#\$\%\^\&\*\?]).{8,15}$"
+                            title="Must contain at least one  number and one uppercase and one lowercase letter and One special Charecter, and at least 8 characters" 
+                            placeholder ='Confirm New Password' 
+                            required 
+                        />
+                        {showConfirmPassword ? <IoEyeOutline className='iconle' onClick={toggleConfirmPasswordVisibility} /> : <IoEyeOffOutline className='iconl' onClick={toggleConfirmPasswordVisibility} />}
+                    </div>
+                
+                </div>
+                <div>
+                    <button type='submit' className='buttonf'>Reset Password</button>
+                </div>
+            </form>
+            <Toaster toastOptions={{style: customToastStyle,duration:1500,}} position="top-center" reverseOrder={false} />
+        </div>
+    );
+};
+
+export default ForgotPassword;
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,162 @@
+import React, { useState, useEffect } from 'react';
+import './Login.css';
+import config from './config'; 
+import { useHistory,useLocation } from 'react-router-dom';
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5"; 
+import { CgMail } from 'react-icons/cg';
+import { toast, Toaster } from 'react-hot-toast';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false); 
+    const [id, setId] = useState();
+    const navigate = useHistory();
+    const [rememberMe, setRememberMe] = useState(false); // Add rememberMe state
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('rememberedUser');
+        const storedPassword = localStorage.getItem('rememberedPassword');
+        if (storedUser && storedPassword) {
+            setEmail(storedUser);
+            setPassword(storedPassword);
+            setRememberMe(true); // Set rememberMe state when rememberedUser and rememberedPassword are found
+        }
+    }, []);
+
+    const handleRememberMeChange = () => {
+        setRememberMe(!rememberMe);
+    };
+
+useEffect(() => {
+        const registeredEmail = localStorage.getItem('registeredEmail');
+        if (registeredEmail) {
+            setEmail(registeredEmail);
+            localStorage.removeItem('registeredEmail'); // Remove the email after fetching it
+        }
+     }, []); // Empty dependency array means this effect runs only once when component mounts
+
+     useEffect(() => {
+        const fetchUserName = async () => {
+                const response = await fetch(`${config.ApiUrl}User/GetUserName${email}`);
+                if (response.ok) {
+                    const userData = await response.json();
+                    setId(userData.id);
+                } 
+        };
+        if(email)
+        {
+            fetchUserName();
+        }
+    }, [email]);
+
+const handleUserChange = (e) => {
+    setEmail(e.target.value);
+};
+    
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const togglePasswordVisibility = () => { 
+        setShowPassword(!showPassword);
+    };
+        
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${config.ApiUrl}User/Login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+            });
+            if (response.ok) {
+
+                const data = await response.json();
+                setPassword(data.password);
+                localStorage.setItem('loggedInEmail', email);
+                if (rememberMe) {
+                    localStorage.setItem('rememberedUser', email);
+                    localStorage.setItem('rememberedPassword', password);
+                } else {
+                    localStorage.removeItem('rememberedUser');
+                    localStorage.removeItem('rememberedPassword');
+                }
+
+                // Delay navigation to ensure toast message is visible
+                setTimeout(() => {
+                    navigate.push('/UpdateUserDetail'); 
+                }, 1500); 
+                
+                toast.success("Login Successfully!")
+                localStorage.setItem("loggedEmail",JSON.stringify({
+                    email,
+                    id
+                  }));
+        
+            } else if (response.status === 401) {
+                const errorMessage = await response.text();
+                toast.error(errorMessage);
+            } else {
+                toast.error('Login failed. Please try again later.');
+            }
+        } catch (error) {
+            console.error('Login failed:', error);
+            toast.error('Login failed');
+        }
+    };
+
+    const customToastStyle = {
+        fontFamily: "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
+        fontSize: '16px',
+        fontWeight: 'bold',
+      };
+
+    return (
+        <div className='containerl'>
+            <form onSubmit={handleSubmit}>
+                <h2>Login Form</h2>
+                <input type='hidden' value={id}/>
+                <div className='form-groupl'>
+                    <label className='labell'>Email:</label>
+                    <input
+                        className='inputl'
+                        type='email'
+                        value={ email}
+                        onChange={handleUserChange}
+                        placeholder='Enter Your Email'
+                        required
+                    /> 
+                </div>
+                <CgMail className='icone'/>
+                <div className='form-groupl'>
+                    <label className='labell'>Password:</label>
+                    <div className='password-input'>
+                    <input className='inputl' type={showPassword ? 'text' : 'password'} value={password}     autoComplete="current-password"
+                            onChange={handlePasswordChange} placeholder='Enter Your Password'
+                         pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~\@\!\#\$\%\^\&\*\?]).{8,15}$"
+                         title="Must contain at least one  number and one uppercase and one lowercase letter and One special Charecter, and at least 8 characters"
+                         required />
+                        {showPassword ? <IoEyeOutline className='iconl' onClick={togglePasswordVisibility} /> : <IoEyeOffOutline className='iconl' onClick={togglePasswordVisibility} />}
+                    </div>
+                </div>
+                <div className='forgotl'>
+                     <input type='checkbox' checked={rememberMe} onChange={handleRememberMeChange} /><span>Remember me</span>
+
+                    <a href='ForgotPassword' className='f'>Forgot Password?</a>
+                </div>
+                <div>
+                    <button type='submit' className='button'> Login </button>
+                </div>
+                <div className='register-link'>
+                <p className='p'>Don't have an account? <a href='Registration' className='f'>Register</a></p>
+            </div>
+            </form>
+            <Toaster toastOptions={{style: customToastStyle,duration:1500,}} position="top-center" reverseOrder={false} />
+        </div>
+    );
+};
+
+export default Login;
