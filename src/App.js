9f3693f870792cda5a5e41f3d707a9797a151734
AyushiDomadiya <ayushidomadiya.2151@gmail.com>
2024-03-20 13:12:35 +0530

    Registration_ConnectionDb

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React,{useState,useEffect} from 'react';
+//import React,{useState,useEffect} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './Components/Login/Login';
 import Registration from './Components/Login/Registration'; 
 import ForgotPassword from './Components/Login/ForgotPassword';
@@ -10,7 +10,16 @@ import JsonData from './Components/Pages/JsonData';
 import Userdetail from './Components/Pages/Userdetail';
 import UpdateUserdetail from './Components/Pages/UpdateUserdetail';
 import Familydetail from './Components/Pages/Familydetail';
-
+import StudentForm from './Components/Admin/StudentForm';
+import TeacherForm from './Components/Admin/TeacherForm';
+import Student_Form from './Components/Teacher/Student_Form';
+import Standard from './Components/Teacher/Standard';
+import TimeTable from './Components/Teacher/TimeTable';
+import SubjectForm from './Components/Teacher/SubjectForm';
+import Student from './Components/Student/Student';
+import FamilyForm from './Components/Admin/FamilyForm'
+import FeeForm from './Components/Admin/FeeForm';
+import StudentFeeForm from './Components/Student/StudentFeeForm';
 function App() {
  
   
@@ -27,6 +36,17 @@ function App() {
           <Route path='/JsonData' component={JsonData}/>
           <Route path='/ForgotPassword' component={ForgotPassword}/>
           <Route path='/Registration' component={Registration}/>
+          <Route path='/StudentForm' component={StudentForm}/>
+          <Route path='/TeacherForm' component ={TeacherForm}/>
+          <Route path='/Student_Form' component ={Student_Form}/>
+          <Route path='/TimeTable' component={TimeTable}/>
+          <Route path='/Standard' component={Standard}/>
+          <Route path='/SubjectForm' component={SubjectForm}/>
+          <Route path='/Student' component={Student}/>
+          <Route path='/FamilyForm' component={FamilyForm}/>
+          <Route path='/FeeForm' component={FeeForm}/>
+          <Route path='/StudentFeeForm' component={StudentFeeForm}/>
+
           <Route path='/' component={Login}/>
         </Switch>
       </Router>
