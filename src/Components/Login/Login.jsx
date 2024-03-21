9813fc2559e1d98f405a36dbd3f588792d933a34
hardikjadav27 <hardikjadav1227@gmail.com>
2024-03-21 10:27:30 +0530

    Login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,179 +1,101 @@
-import React, { useState, useEffect } from 'react';
-import './Login.css';
-import config from './config'; 
+import React, { useState } from 'react';
+import './Sidebar.css';
+import { NavLink } from 'react-router-dom';
+import { BiSolidUserDetail, BiLogOut } from 'react-icons/bi';
+import { MdFamilyRestroom } from 'react-icons/md';
+import { RiDatabase2Line } from 'react-icons/ri';
+import { FaBars } from 'react-icons/fa';
 import { useHistory } from 'react-router-dom';
-import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5"; 
-import { CgMail } from 'react-icons/cg';
-import { toast, Toaster } from 'react-hot-toast';
+import user_icon from '../Assets/user_icon.png';
+import logout from '../Assets/logout.png';
 
-const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false); 
-    const [id, setId] = useState();
+const Sidebar = ({ children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
     const navigate = useHistory();
-    const [role, setRole] = useState();
-    const [rememberMe, setRememberMe] = useState(false); // Add rememberMe state
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
-    useEffect(() => {
-        const storedUser = localStorage.getItem('rememberedUser');
-        const storedPassword = localStorage.getItem('rememberedPassword');
-        if (storedUser && storedPassword) {
-            setEmail(storedUser);
-            setPassword(storedPassword);
-            setRememberMe(true); // Set rememberMe state when rememberedUser and rememberedPassword are found
-        }
-    }, []);
-
-    const handleRememberMeChange = () => {
-        setRememberMe(!rememberMe);
-    };
-
-useEffect(() => {
-        const registeredEmail = localStorage.getItem('registeredEmail');
-        if (registeredEmail) {
-            setEmail(registeredEmail);
-            localStorage.removeItem('registeredEmail'); // Remove the email after fetching it
-        }
-     }, []); // Empty dependency array means this effect runs only once when component mounts
-
-     useEffect(() => {
-        const fetchUserName = async () => {
-                const response = await fetch(`${config.ApiUrl}User/GetUserName${email}`);
-                if (response.ok) {
-                    const userData = await response.json();
-                    setId(userData.id);
-                } 
-        };
-        if(email)
+    const menuItem = [
         {
-            fetchUserName();
+            path: '/UpdateUserdetail',
+            name: 'User Detail',
+            icon: <BiSolidUserDetail />
+        },
+        {
+            path: '/Familydetail',
+            name: 'Family',
+            icon: <MdFamilyRestroom />
+        },
+        {
+            path: '/JsonData',
+            name: 'Json Data',
+            icon: <RiDatabase2Line />
+        },
+        {
+            path: '/login',
+            name: 'Logout',
+            icon: <BiLogOut />
         }
-    }, [email]);
+    ];
 
-const handleUserChange = (e) => {
-    setEmail(e.target.value);
-};
-    
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
+    const handleLogout = () => {
+        localStorage.removeItem('loggedInEmail');
+        navigate.push('/');
     };
-
-    const togglePasswordVisibility = () => { 
-        setShowPassword(!showPassword);
-    };
-        
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (!role) {
-            toast.error('Please select your role!');
-            return;
-          }
-        
-        try {
-            const response = await fetch(`${config.ApiUrl}User/Login`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ email, password })
-            });
-            if (response.ok) {
-
-                const data = await response.json();
-                setPassword(data.password);
-                localStorage.setItem('loggedInEmail', email);
-                if (rememberMe) {
-                    localStorage.setItem('rememberedUser', email);
-                    localStorage.setItem('rememberedPassword', password);
-                } else {
-                    localStorage.removeItem('rememberedUser');
-                    localStorage.removeItem('rememberedPassword');
-                }
-
-                // Delay navigation to ensure toast message is visible
-                setTimeout(() => {
-                    navigate.push('/UpdateUserDetail'); 
-                }, 1500); 
-                
-                toast.success("Login Successfully!")
-                localStorage.setItem("loggedEmail",JSON.stringify({
-                    email,
-                    id
-                  }));
-        
-            } else if (response.status === 401) {
-                const errorMessage = await response.text();
-                toast.error(errorMessage);
-            } else {
-                toast.error('Login failed. Please try again later.');
-            }
-        } catch (error) {
-            console.error('Login failed:', error);
-            toast.error('Login failed');
-        }
-    };
-
-    const customToastStyle = {
-        fontFamily: "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
-        fontSize: '16px',
-        fontWeight: 'bold',
-      };
 
     return (
-        <div className='containerl'>
-            <form onSubmit={handleSubmit}>
-                <h2>Login Form</h2>
-                <input type='hidden' value={id}/>
-                <div className='form-groupl'>
-                    <label className='labellogin'>User Role</label>
-                    <div className='radio-group3'>
-                    <input className='inputr' type="radio" name="role" id="admin" value={1} onChange={e => setRole(e.target.value)} />
-                    <label htmlFor="administrator">Admin</label>
-                    <input className="form-check-input" type="radio" name="role" id="teacher" value={2} onChange={e => setRole(e.target.value)} />
-                    <label htmlFor="staff">Teacher</label>
-                    <input className="form-check-input" type="radio" name="role" id="student" value={3} onChange={e => setRole(e.target.value)} />
-                    <label htmlFor="user">Student</label>
+        <>
+             <div className="menu-container">
+                <div className="menu-trigger">
+                    <h2>Welcome To Web</h2>
+                    <div
+                        className="user-icon-container"
+                        onMouseEnter={() => setDropdownOpen(true)}
+                        onMouseLeave={() => setDropdownOpen(false)}
+                    >
+                        <img src={user_icon} alt="" />
+                        {dropdownOpen && (
+                            <div className="dropdown-menu" onMouseEnter={() => setDropdownOpen(true)} onMouseLeave={() => setDropdownOpen(false)}>
+                                <ul>
+                                    <li>
+                                        <img className='user_icon' src={user_icon} alt="" />
+                                        <h3>Daxa Chhatrodiya</h3>
+                                    </li>
+                                    <li onClick={handleLogout}>
+                                    <img className='logout_icon' src={logout} alt="" />
+                                    <a href="/">Logout</a>
+                                </li>
+                                </ul>
+                            </div>
+                        )}
                     </div>
                 </div>
-                <div className='form-groupl'>
-                    <label className='labell'>Email:</label>
-                    <input
-                        className='inputl'
-                        type='email'
-                        value={ email}
-                        onChange={handleUserChange}
-                        placeholder='Enter Your Email'
-                        required
-                    /> 
-                </div>
-                <CgMail className='icone'/>
-                <div className='form-groupl'>
-                    <label className='labell'>Password:</label>
-                    <div className='password-input'>
-                    <input className='inputl' type={showPassword ? 'text' : 'password'} value={password}     autoComplete="current-password"
-                            onChange={handlePasswordChange} placeholder='Enter Your Password'
-                         pattern="^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[~\@\!\#\$\%\^\&\\?]).{8,15}$"
-                         title="Must contain at least one  number and one uppercase and one lowercase letter and One special Charecter, and at least 8 characters"
-                         required />
-                        {showPassword ? <IoEyeOutline className='iconl' onClick={togglePasswordVisibility} /> : <IoEyeOffOutline className='iconl' onClick={togglePasswordVisibility} />}
-                    </div>
-                </div>
-                <div className='forgotl'>
-                     <input type='checkbox' checked={rememberMe} onChange={handleRememberMeChange} /><span>Remember me</span>
-
-                    <a href='ForgotPassword' className='f'>Forgot Password?</a>
-                </div>
-                <div>
-                    <button type='submit' className='button'> Login </button>
-                </div>
-                <div className='register-link'>
-                <p className='p'>Don't have an account? <a href='Registration' className='f'>Register</a></p>
             </div>
-            </form>
-            <Toaster toastOptions={{style: customToastStyle,duration:1500,}} position="top-center" reverseOrder={false} />
-        </div>
+            <div className="containers">
+                <div style={{ width: isOpen ? '300px' : '60px' }} className="sidebar">
+                    <div className="top-section">
+                        <h1 style={{ display: isOpen ? 'block' : 'none' }} className="logo">
+                            Daxa
+                            <br /> <span>Chhatrodiya</span>
+                        </h1>
+                        <div style={{ marginLeft: isOpen ? '30px' : '0px' }} className="bars">
+                            <FaBars onClick={toggle} />
+                        </div>
+                    </div>
+                    {menuItem.map((item, index) => (
+                        <NavLink to={item.path} key={index} className="link" activeClassName="active">
+                            <div className="icons">{item.icon}</div>
+                            <div style={{ display: isOpen ? 'block' : 'none' }} className="link-text">
+                                {item.name}
+                            </div>
+                        </NavLink>
+                    ))}
+                </div>
+                <main>{children}</main>
+            </div>
+        </>
     );
 };
 
-export default Login;
+export default Sidebar;
+
