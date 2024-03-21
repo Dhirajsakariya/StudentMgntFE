fc6cbdc5f1bb1e0964667c4f26be15564ed07371
BansiBhanvadiya <bhanvadiyabansi2002@gmail.com>
2024-03-21 17:00:33 +0530

    commit

--- src/Components/Teacher/Standard.jsx
+++ src/Components/Teacher/Standard.jsx
@@ -1,28 +1,97 @@
-import React from 'react'
+// import React, { useEffect, useState } from 'react'
+// import '../Teacher/Standard.css';
+// import config from '../Login/config';
+
+// const Standard = () => {
+
+//   const [data, setData] =useState([]);
+// useEffect(() =>{
+//   const fetchData = async()=>{
+//     try{
+//       const response = await fetch(`${config.ApiUrl}Standard/GetStandards`);
+
+//       if(!response.ok){
+//       const result = await response.json();
+//       setData(result);
+//       console.log(result);
+//       }
+//     }catch(error){
+//       console.log("Failed to fetch standard data!");
+//     }
+//   }
+//   fetchData();
+// },[])
+
+//   return (
+//     <div>
+//       <div className='standard'>
+//         <h1 className='standard-heading'>Standard List</h1>
+//         <div>
+//             <table className='tables'>
+//                 <tr>
+//                     <th>Standard</th>
+//                     <th>Section</th>
+//                 </tr>                
+//             </table>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Standard
+
+import React, { useEffect, useState } from 'react';
 import '../Teacher/Standard.css';
+import config from '../Login/config';
 
 const Standard = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${config.ApiUrl}Standard/GetStandards`);
+
+        if (response.ok) {
+          const result = await response.json();
+          setData(result);
+          console.log(result);
+        }
+      } catch (error) {
+        console.log("Failed to fetch standard data!");
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div className='standard'>
         <h1 className='standard-heading'>Standard List</h1>
         <div>
-            <table className='tables'>
-                <tr >
-                    <th>No.</th>
-                    <th>Standard</th>
-                    <th>Section</th>
+          <table className='tables'>
+            <thead> {/* Add thead element */}
+              <tr>
+                <th>Standard</th>
+                <th>Section</th>
+              </tr>
+            </thead>
+            <tbody> {/* Add tbody element */}
+              {/* Map over your data array and render rows */}
+              {data.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.standardNumber}</td>
+                  <td>{item.section}</td>
                 </tr>
-                <tr >
-                <td>1</td>
-                <td>1st</td>
-                <td>A</td>
-                </tr>
-            </table>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Standard
+export default Standard;
