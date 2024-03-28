1cfa822118ba07c496bf433c610a45d7f24f992b
Madhuri191 <madhurisanchania10@gmail.com>
2024-03-28 10:04:04 +0530

    Student_form css

--- src/Components/Teacher/Student_Form.jsx
+++ src/Components/Teacher/Student_Form.jsx
@@ -1,13 +1,13 @@
-import React,{useState} from 'react'
 import moment from 'moment';
-import './Student_Form.css'
+import React, { useState } from 'react';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 import TeacherSidebar from '../Sidebar/TeacherSidebar';
+import './Student_Form.css';
 
 export const Student_Form = () => {
 
-      
+    const [GrNo, setGrNo] = useState('')
     const [rollNo,setRollNo] =useState('');
     const [name, setName ] = useState('');
     const [email, setEmail] = useState('');
@@ -53,31 +53,36 @@ export const Student_Form = () => {
     <div className='containerST'>
         <form>
             <div>
-            <h2 className='student-form'>Student Detail</h2>
-            <div className='form-groupstd'>
-                <label className='labelstd'>Roll  No:</label>
-                <input className='inputstd' type='text' value={rollNo} onChange={(e)=> setRollNo(e.target.value)} placeholder='Enter RollNo'
+            <h2 id='student-form'>Student Detail</h2>
+            <div id='form-groupstudent_form'>
+                <label id='student_form'>Gr  No:</label>
+                <input id='inputstudent_form' type='text' value={GrNo} onChange={(e)=> setGrNo(e.target.value)} placeholder='Enter GrNo'
+                name='no'  required />
+            </div>
+            <div id='form-groupstudent_form'>
+                <label id='student_form'>Roll  No:</label>
+                <input id='inputstudent_form' type='text' value={rollNo} onChange={(e)=> setRollNo(e.target.value)} placeholder='Enter RollNo'
                 name='Rollno'  required />
             </div>
-            <div className='form-groupstd'>
-                <label className='labelstd'>Name:</label>
-                <input className='inputstd' type='text' value={name} onChange={(e)=> setName(e.target.value)} placeholder='Enter Name'
+            <div id='form-groupstudent_form'>
+                <label id='student_form'>Name:</label>
+                <input id='inputstudent_form' type='text' value={name} onChange={(e)=> setName(e.target.value)} placeholder='Enter Name'
                 name='name'  required />
             </div>
-            <div className='form-groupstd'>
-                <label className='labelstd'>Email:</label>
-                <input className='inputstd' type='email' value={email} onChange={(e)=> setEmail(e.target.value)} placeholder='Enter Email'
+            <div id='form-groupstudent_form'>
+                <label id='student_form'>Email:</label>
+                <input id='inputstudent_form' type='email' value={email} onChange={(e)=> setEmail(e.target.value)} placeholder='Enter Email'
                 name='Email'  required />
             </div>
-            <div className='form-groupstd'>
-                <label className='labelstd'>Password:</label>
-                <input className='inputstd' type='password' value={password} onChange={(e)=> setPassword(e.target.value)} placeholder='Password'
+            <div id='form-groupstudent_form'>
+                <label id='student_form'>Password:</label>
+                <input id='inputstudent_form' type='password' value={password} onChange={(e)=> setPassword(e.target.value)} placeholder='Password'
                 name='Password'  required />
             </div>
-            <div className='form-groupstd'>
-            <label className='labelstd'>Gender:</label>
-            <div className="radio-groupstd">
-                   <input className='inputstd'
+            <div id='form-groupstudent_form'>
+            <label id='student_form'>Gender:</label>
+            <div id="radio-groupstd">
+                   <input id='inputstudent_form'
                      type="radio"
                      value="male"
                  checked={gender === "male"}
@@ -95,40 +100,41 @@ export const Student_Form = () => {
                    <label>Female</label>
                  </div>
             </div>
-            <div className='form-groupstd'>
-                <label className='labelstd'>DOB:</label>
-                <input className='inputstd' type='date' value={birthday} max={moment().format("YYYY-MM-DD")} onChange={(e) => setBirthday(e.target.value)} required />  
+            <div id='form-groupstudent_form'>
+                <label id='student_form'>DOB:</label>
+                <input id='inputstudent_form' type='date' value={birthday} max={moment().format("YYYY-MM-DD")} onChange={(e) => setBirthday(e.target.value)} required />  
             </div>
-            <div className='form-groupstd'>
-                <label className='labelstd'>Join-Date:</label>
-                <input className='inputstd' type='date' value={joinDate} max={moment().format("YYYY-MM-DD")} onChange={(e) => setJoinDate(e.target.value)} required />
+            
+            <div id='form-groupstudent_form'>
+                <label id='student_form'>Join-Date:</label>
+                <input id='inputstudent_form' type='date' value={joinDate} max={moment().format("YYYY-MM-DD")} onChange={(e) => setJoinDate(e.target.value)} required />
             </div>
             </div>
     
-        <div className='div_two'>           
+        <div id='div_two'>           
         <div>
-            <label className='labelh' htmlFor="bloodgroup">Select a BloodGroup:</label>
-                 <select className='inputh'  value={selectedBloodGroup} onChange={handleBloodGroupChange}>
+            <label id='student_form2' htmlFor="bloodgroup">Select a BloodGroup:</label>
+                 <select id='inputstudent_form2'  value={selectedBloodGroup} onChange={handleBloodGroupChange}>
                  <option value="">--Select BloodGroup--</option>
                   {bloodGroup.map((bloodGroup, index) => (
                   <option key={index} value={bloodGroup}>{bloodGroup}</option>
                    ))}
                  </select>
            </div>
-            <div className='form-grouph'>
-                <label className='labelh'>Address:</label>
-                <textarea className='inputh'  value={address} onChange={(e)=> setAddress(e.target.value)} placeholder='Address'
+            <div id='form-groupstudent_form'>
+                <label id='student_form2'>Address:</label>
+                <textarea id='inputstudent_form2'  value={address} onChange={(e)=> setAddress(e.target.value)} placeholder='Address'
                 name='Address'  required />
             </div> 
-            <div className='form-grouph'>
-                <label className='labelh'>City:</label>
-                <input className='inputh' type='text' value={city} onChange={(e)=> setCity(e.target.value)} placeholder='Enter Your City'
+            <div id='form-groupstudent_form'>
+                <label id='student_form2'>City:</label>
+                <input id='inputstudent_form2' type='text' value={city} onChange={(e)=> setCity(e.target.value)} placeholder='Enter Your City'
                 name='city'  required />
             </div>
            
             <div>
-            <label className='labelh' htmlFor="district">Select a district:</label>
-                 <select className='inputh'  value={selectedDistrict} onChange={handleDistrictChange}>
+            <label id='student_form2' htmlFor="district">Select a district:</label>
+                 <select id='inputstudent_form2'  value={selectedDistrict} onChange={handleDistrictChange}>
                  <option value="">--Select District--</option>
                   {districts.map((district, index) => (
                   <option key={index} value={district}>{district}</option>
@@ -136,15 +142,15 @@ export const Student_Form = () => {
                  </select>
                  {/* {selectedDistrict &&  <p style={{color:'black',position:'right'}}>You selected: {selectedDistrict}</p>} */}
            </div>
-           <div className='form-grouph'>
-                <label className='labelh'>State:</label>
-                <input className='inputh' type='text' value='Gujarat' onChange={(e)=> setState(e.target.value)}
+           <div id='form-groupstudent_form'>
+                <label id='student_form2'>State:</label>
+                <input id='inputstudent_form2' type='text' value='Gujarat' onChange={(e)=> setState(e.target.value)}
                 name='city'  required />
             </div>
            
-           <div className='form-grouph'>
-               <label className='labelh'>Mobile Number:</label>
-               <div className='phone_numberS'>
+           <div id='form-groupstudent_form'>
+               <label id='student_form2'>Mobile Number:</label>
+               <div id='phone_numberS'>
                <PhoneInput
                    country={'in'}
                    value={mobileNumber}
@@ -157,7 +163,7 @@ export const Student_Form = () => {
                  </div>
                </div>  
         </div>
-            <button className='btnnexts' type='submit'>Next</button>
+            <button id='btnnexts' type='submit'>Next</button>
          </form>
     </div>
     </>
