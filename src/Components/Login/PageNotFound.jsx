8ed1bd83c07a7bb428bdc7adc6171a031d76a014
hardikjadav27 <hardikjadav1227@gmail.com>
2024-04-03 12:28:39 +0530

    page-not-sound-css

--- src/Components/Login/PageNotFound.jsx
+++ src/Components/Login/PageNotFound.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import './PageNotFound.css';
 
+const PageNotFound = () => {
+  return (
+    <div className="page-not-fopund">
+      <h1 className="h1-page-not-found">404 - Not Found</h1>
+      <p className="text-page-not-found">The page you're looking for does not exist.</p>
+      <Link to="/" className="link-page-not-found">Go to Home</Link>
+      <br />
+      <img src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif" alt="404 Not Found" className="image-page-not-found" />
+    </div>
+  );
+};
 
- const PageNotFound = () => {
-    return (
-        <div>
-          <h1>404 - Not Found</h1>
-          <p>The page you're looking for does not exist.</p>
-          <Link to="/">Go to Home</Link>
-          <br />
-          <img src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif" alt="404 Not Found" />
-        </div>
-      );
-    };
-
-    export default PageNotFound;
-
-    
+export default PageNotFound;
