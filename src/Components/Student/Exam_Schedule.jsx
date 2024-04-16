acfb5b3df2db7851939bbac630590ae4f11c360f
RamaniJanvi <ramanijanvi946@gmail.com>
2024-04-16 10:57:23 +0530

    complete exam Schedule on student side complete exam Schedule on student side

--- src/Components/Student/Exam_Schedule.jsx
+++ src/Components/Student/Exam_Schedule.jsx
@@ -6,21 +6,22 @@ import toast, { Toaster } from 'react-hot-toast';
 import { Redirect } from 'react-router-dom';
 
 const Exam_Schedule = () => {
-    const [timeTables, setTimeTables] = useState([]);
+    const [examSchedule, setExamSchedule] = useState([]);
     const [studentId, setStudentId] = useState('');
     const [standardId, setStandardId] = useState('');
+    const [selectedExamType, setSelectedExamType] = useState('');
     const [redirectToNotFound, setRedirectToNotFound] = useState(false);
 
     useEffect(() => {
         const fetchStudentData = async () => {
             try {
-                const storedId = JSON.parse(localStorage.getItem('loggedInUserId'));
-                if (storedId) {
-                    setStudentId(storedId);
-                } else {
-                    console.error('loggedInUserId not found in localStorage');
-                }
-            } catch (error) {
+                    const storedId = JSON.parse(localStorage.getItem('loggedInUserId'));
+                    if (storedId) {
+                        setStudentId(storedId);
+                    } else {
+                        console.error('loggedInUserId not found in localStorage');
+                    }
+                } catch (error) {
                 console.error('Error fetching student data:', error);
                 toast.error('Error fetching student data');
             }
@@ -31,14 +32,14 @@ const Exam_Schedule = () => {
     useEffect(() => {
         const fetchStandardData = async () => {
             try {
-                const response = await fetch(`${config.ApiUrl}Student/GetStudent/${studentId}`);
-                if (response.ok) {
-                    const data = await response.json();
-                    setStandardId(data.standardId);
-                } else {
+                    const response = await fetch(`${config.ApiUrl}Student/GetStudent/${studentId}`);
+                    if (response.ok) {
+                        const data = await response.json();
+                        setStandardId(data.standardId);
+                    } else {
                     toast.error('Failed to fetch standard');
-                }
-            } catch (error) {
+                    }
+                } catch (error) {
                 console.error('Error fetching standard:', error);
                 toast.error('Error fetching standard');
             }
@@ -49,24 +50,29 @@ const Exam_Schedule = () => {
     }, [studentId]);
 
     useEffect(() => {
-        const fetchTimeTable = async () => {
+        const fetchExamSchedule = async () => {
             try {
-                const response = await fetch(`${config.ApiUrl}Exam/GetExamByStandardId/${standardId}`);
-                if (response.ok) {
+                    const response = await fetch(`${config.ApiUrl}Exam/GetExamByStandardId/${standardId}`);
+                    if (response.ok) {
                     const data = await response.json();
-                    setTimeTables(data);
-                } else {
-                    toast.error('Failed to fetch timetable');
-                }
-            } catch (error) {
-                console.error('Error fetching timetable:', error);
-                toast.error('Error fetching timetable');
+                    setExamSchedule(data);
+                    } else {
+                        toast.error('Failed to fetch Exam-Schedule');
+                    }
+                } catch (error) {
+                console.error('Error fetching Exam-Schedule:', error);
+                toast.error('Error fetching Exam-Schedule');
             }
         };
         if (standardId) {
-            fetchTimeTable();
+            fetchExamSchedule();
         }
     }, [standardId]);
+
+    // Filter timeTables based on selected exam type
+    const filteredExamSchedule = selectedExamType
+        ? examSchedule.filter(schedule => schedule.examType === selectedExamType)
+        : examSchedule;
 
     if (redirectToNotFound) {
         return <Redirect to="/PageNotFound" />;
@@ -75,33 +81,43 @@ const Exam_Schedule = () => {
     return (
         <>
             <StudentSidebar>
-                <div>
+                <div> 
                     <div id='examviewer'>
                         <h1 id='h1examschedule'>Exam Schedule</h1>
                     </div>
                     <div id='exam-scheduletable'>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th id='thtableexam'>Exam_Type</th>
-                                <th id='thtableexam'>Date</th>
-                                <th id='thtableexam'>Subject</th>
-                                <th id='thtableexam'>Time</th>
-                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {timeTables.map((schedule, index) => (
-                                <tr key={index}>
-                                    <td id='exam-th'>{schedule.examType}</td>
-                                    <td id='exam-th'>{schedule.examDate.split("-").reverse().join("-")}</td>
-                                    <td id='exam-th'>{schedule.subject}</td>
-                                    <td id='exam-th'>{schedule.startTime} - {schedule.endTime}</td>
-                                   
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                        <div>
+                            <select id="examdropdown" onChange={e => setSelectedExamType(e.target.value)}>
+                                <option value="" disabled >Select Exam-Type</option>
+                                <option value="">All</option>
+                                <option value="MidTerm">MidTerm</option>
+                                <option value="Final">Final</option>
+                            </select>
+                        </div>
+                        <table>
+                            <thead>
+                             <tr>
+                                 {selectedExamType === "" && (
+                                    <th id='thtableexam'>Exam_Type</th>
+                                 )}
+                                    <th id='thtableexam'>Date</th>
+                                    <th id='thtableexam'>Subject</th>
+                                    <th id='thtableexam'>Time</th>
+                             </tr>
+                            </thead>
+                              <tbody>
+                                {filteredExamSchedule.map((schedule, index) => (
+                                    <tr key={index}>
+                                          {selectedExamType === "" && (
+                                        <td id='exam-th'>{schedule.examType}</td>
+                                    )}
+                                        <td id='exam-th'>{schedule.examDate.split("-").reverse().join("-")}</td>
+                                        <td id='exam-th'>{schedule.subject}</td>
+                                        <td id='exam-th'>{schedule.startTime} - {schedule.endTime}</td>
+                                    </tr>
+                                ))}
+                              </tbody>
+                         </table>
                     </div>
                 </div>
             </StudentSidebar>
