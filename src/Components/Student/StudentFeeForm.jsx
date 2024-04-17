3653da18f929a182e90ad8508f3a769d2aaac403
hardikjadav27 <hardikjadav1227@gmail.com>
2024-04-17 17:24:56 +0530

    student side fees show with css done

--- src/Components/Student/StudentFeeForm.jsx
+++ src/Components/Student/StudentFeeForm.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast, Toaster } from 'react-hot-toast';
 import { FaRupeeSign } from "react-icons/fa";
 import StudentSidebar from '../Sidebar/StudentSidebar';
+import Popup from 'reactjs-popup';
 import './StudentFeeForm.css';
 import config from '../Login/config';
 
@@ -11,11 +12,18 @@ const StudentFeeForm = () => {
   const [feeFrequency, setFeeFrequency] = useState('');
   const feeFrequencies = ["Quarterly", "Annually", "Semi-Annually"];
   const [selectedFrequency,setselectedFrequency]=useState('');
+  const [selectfeeFrequency , setSelectFeeFrequency] = useState('')
   const [message, setMessage] = useState('');
+  const [studentName, setStudentName] = useState('');
+  const [pendingAmount,setPendingAmount] = useState(null);
+  const [showModal, setShowModal] = useState(false); 
+  const [paidFees, setPaidFees] = useState('');
 
-
+  
   const handleFrequencyChange = async (e) => {
     const selectedFrequency = e.target.value;
+    setselectedFrequency(selectedFrequency);
+
 
     // Fetch fee amount from backend based on selected frequency
     const response = await fetch(`${config.ApiUrl}Fees/GetFeeAmount/${fullId}?frequency=${selectedFrequency}`, {
@@ -33,11 +41,65 @@ const StudentFeeForm = () => {
     }
   };
 
+  useEffect(() => {
+
+    const fetchStudentName =async () => {
+      try{
+        const response =  await fetch(`${config.ApiUrl}Student/GetStudent/${fullId}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch student details');
+        }
+        const studentData = await response.json();
+        setStudentName(studentData.name);
+            
+  
+      } catch (error) {
+        console.error('Error fetching student name:', error);
+      }
+    };
+    fetchStudentName();
+   }, []);
+
+  const handleView = () => {
+
+    fetchStudentFeesDetails(true);
+    setShowModal(true);
+  };
+
+  const fetchStudentFeesDetails = async (showFeesView) => {
+    try {
+      const response = await fetch(`${config.ApiUrl}Fees/GetStudentFeesDetails/${fullId}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch student fees details');
+      }
+      const feesData = await response.json();
+      console.log('feesData ', feesData, feesData.paidFees);
+      setPaidFees(feesData.paidFees);
+      // setPendingFees(feesData.pendingFees);
+      if(showFeesView){
+        setShowModal(true);
+      }
+    } catch (error) {
+      console.error('Error fetching student fees details:', error);
+    }
+  };
+
+
   const studentObject = JSON.parse(localStorage.getItem('loggedInUserId'));
   const fullId = studentObject;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+
+    
+    if (!feeFrequency) {
+      if(!selectfeeFrequency){
+      toast.error('Please select a frequency.');
+      }
+      setSelectFeeFrequency(true)
+      return; 
+    }
 
     try {
       const response = await axios.post(`${config.ApiUrl}Fees/PostFees`, {
@@ -52,7 +114,7 @@ const StudentFeeForm = () => {
       setFeeFrequency('');
       setFeeAmount('');
       const userERes = response.data;
-      
+      setPendingAmount(userERes.remainingAmount);   
 
      if (userERes.remainingAmount > 0) {
         // Display toast message with remaining amount and updated frequencies
@@ -126,8 +188,26 @@ const StudentFeeForm = () => {
             <br />
           </div>
           <button id="buttontypee" type="submit">Pay</button>
-          <br /><br />
+      
+          <button onClick={handleView}id="buttontypeview" >View</button>
           <Toaster toastOptions={{ className: "custom-toast", style: customToastStyle, duration: 4500, }} position="top-center" reverseOrder={false} />
+          <Popup
+        contentStyle={{ width: "400px" , height:'350px', borderRadius:'10px', background:'#f7f9fb' }}
+        open={showModal}
+        onClose={() => setShowModal(false)}
+        closeOnDocumentClick={false} 
+        closeOnEscape={false} 
+      >
+        <div id="popup-content" className='fees-student'>
+          <h2 className='details-student-fees'> {studentName} </h2>
+        
+         <p id='popup-content' className='p-student-fees-form'> Paid fees : {paidFees}</p> 
+         <p id='popup-content' className='p-student-fees-form'> Pending Fees : {pendingAmount} </p>
+          <button id="close-btn-fees" onClick={() => setShowModal(false)}> × </button>
+         
+        </div>
+      </Popup>
+       
         </form>
       </div>
     </StudentSidebar>
