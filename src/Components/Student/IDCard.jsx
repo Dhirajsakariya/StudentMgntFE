66a9f501c8fa8f0d12215f38248cb12d43e69552
BansiBhanvadiya <bhanvadiyabansi2002@gmail.com>
2024-04-26 17:44:20 +0530

    Student IDCard fetch Parents Details

--- src/Components/Student/IDCard.jsx
+++ src/Components/Student/IDCard.jsx
@@ -4,38 +4,18 @@ import config from '../Login/config';
 import { Redirect } from 'react-router-dom';
 import QRCode from 'react-qr-code';
 import StudentSidebar from '../Sidebar/StudentSidebar';
-import axios from 'axios';
 
 const IDCard = () => {
-
-
   const [redirectToNotFound, setRedirectToNotFound] = useState(false);
   const [Student, setStudent] = useState(null);
   const [error, setError] = useState(null);
   const [totalPaidAmount, setTotalPaidAmount] = useState('');
-  const [pendingAmount,setPendingAmount] = useState(null);
+  const [pendingAmount, setPendingAmount] = useState(null);
   const [userPhoto, setUserPhoto] = useState(null);
-  const [parents, setParents] = useState([]);
-  const [loading, setLoading] = useState(true);  
+  const [parents, setParents] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const storedId = JSON.parse(localStorage.getItem('loggedInUserId'));
-
-  const fetchFamilyDetails = async () => {
-    try {
-      const studentId = JSON.parse(localStorage.getItem('loggedInUserId'));
-      if (!studentId) {
-        throw new Error('Student ID not found in local storage');
-      }
-
-      const response = await axios.get(`${config.ApiUrl}Family/GetFamilyByStudentId/${studentId}`);
-      setParents(response);
-    } catch (err) {
-      setError(err.message);
-      console.error("Error fetching family details:", err);
-    } finally {
-      setLoading(false);
-    }
-  }; 
 
   useEffect(() => {
     const userRoleString = localStorage.getItem('loggedInRole');
@@ -53,25 +33,23 @@ const IDCard = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        if (!storedId) {
+          throw new Error('User ID not found in local storage');
+        }
+
         const fetchStudentDetails = async () => {
           try {
-            if (!storedId) {
-              throw new Error('User ID not found in local storage');
-            }
-  
             const response = await fetch(`${config.ApiUrl}Student/GetStudent/${storedId}`);
-  
             if (!response.ok) {
               throw new Error(`Error fetching Student details: ${response.status} ${response.statusText}`);
             }
-  
             const responseData = await response.json();
             setStudent(responseData);
           } catch (fetchError) {
             setError(fetchError.message);
           }
         };
-  
+
         const fetchFeesDetails = async () => {
           try {
             const response = await fetch(`${config.ApiUrl}Fees/GetStudentFeesDetails/${storedId}`);
@@ -87,7 +65,7 @@ const IDCard = () => {
             console.error('There was a problem with the fetch operation:', error);
           }
         };
-  
+
         const fetchUserPhoto = async () => {
           try {
             const photoResponse = await fetch(`${config.ApiUrl}Student/GetPhoto/${storedId}`);
@@ -101,114 +79,85 @@ const IDCard = () => {
             setError(fetchError.message);
           }
         };
-  
-        await Promise.all([fetchStudentDetails(), fetchFeesDetails(), fetchUserPhoto()]);
-        await Promise.all([fetchStudentDetails(), fetchFeesDetails(), fetchUserPhoto(), fetchFamilyDetails()]);
 
+        const fetchParentsDetails = async () => {
+          try {
+            const response = await fetch(`${config.ApiUrl}Family/GetFamilyByStudentId/${storedId}`);
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            const parentData = await response.json();
+            if (parentData) {
+              setParents(parentData);
+            }
+          } catch (error) {
+            console.error('There was a problem with fetching parents details:', error);
+          }
+        };
+
+        await Promise.all([fetchStudentDetails(), fetchFeesDetails(), fetchUserPhoto(), fetchParentsDetails()]);
+        setLoading(false);
       } catch (error) {
         setError(error.message);
       }
     };
-  
+
     fetchData();
   }, []);
-  
- 
 
-  if (error) {
-    return <div>Error: {error}</div>;
+  if (redirectToNotFound) {
+    return <Redirect to="/PageNotFound" />;
   }
 
   if (!Student) {
     return <div>Loading...</div>;
   }
 
-  if (redirectToNotFound) {
-    return <Redirect to="/PageNotFound" />; 
-  }
-
-//   const qrCodeValue = `Roll No: ${Student.rollNo},  Name: ${Student.name}, Number: ${Student.mobileNumber}, Gender: ${Student.gender}, Paid Fees: ${totalPaidAmount}, Pending Fees: ${pendingAmount}`;
-const qrCodeValue = 
-`\nRoll No: ${Student.rollNo}
- \n Name: ${Student.name}
- \n Email: ${Student.email}
- \n Number: ${Student.mobileNumber}
- \n Birth Date: ${Student.birthDate.split("-").reverse().join("-")}
- \n Join Date ${Student.joinDate.split("-").reverse().join("-")} 
- \n bloodGroup ${Student.bloodGroup}
- \n Gender: ${Student.gender}
- \n address: ${Student.address}
- \n City: ${Student.city}
- \n District ${Student.district}
- \n State ${Student.state}
- \n Pincode ${Student.pinCode}
- \n Paid Fees: ${totalPaidAmount}
- \n Pending Fees: ${pendingAmount}
- \n Parents Email id ${parents.email}
- \n Parents Name: ${parents.name}
- \n parents Gender: ${parents.gender}
- \n Mobile Number: ${parents.mobileNumber}
- \n parents Relation: ${parents.relation}
- \n occupation: ${parents.occupation}`;
-
-
+  const qrCodeValue = `
+    \nRoll No: ${Student.rollNo}
+    \nName: ${Student.name}
+    \nEmail: ${Student.email}
+    \nNumber: ${Student.mobileNumber}
+    \nBirth Date: ${Student.birthDate.split("-").reverse().join("-")}
+    \nJoin Date: ${Student.joinDate.split("-").reverse().join("-")}
+    \nBlood Group: ${Student.bloodGroup}
+    \nGender: ${Student.gender}
+    \nAddress: ${Student.address}
+    \nCity: ${Student.city}
+    \nDistrict: ${Student.district}
+    \nState: ${Student.state}
+    \nPincode: ${Student.pinCode}
+    \nPaid Fees: ${totalPaidAmount}
+    \nPending Fees: ${pendingAmount}
+    \nParents:
+    ${parents.map((parent, index) => (
+      `\n  ${index + 1}. Relation: ${parent.relation}\n  Name: ${parent.name}\n Email: ${parent.email}\n Occupation: ${parent.occupation}\n Gender: ${parent.gender}\n Mobile Number: ${parent.mobileNumber}\n`
+    )).join('')}`;
   return (
     <>
-    <StudentSidebar>
-    <div className="id-card_student">
+      <StudentSidebar>
+        <div className="id-card_student">
 
-      <div className="idcard-school_student">
-      <strong>  STUDENT </strong>
-      </div>
+          <div className="idcard-school_student">
+            <strong>  STUDENT </strong>
+          </div>
 
-        {/* <div className="id-card-body">
-        <strong>Roll No:</strong> 
-        {Student.rollNo}
-      </div>
-       
-       
+          <div>
+            <img id="id-card-photo_student" src={userPhoto} alt="" />
+          </div>
+
+          <div className="id-card-name_student">
+            <h2 className='id-card-header_student'>{Student.name}</h2>
+          </div> 
+
+          <div className="id-card-body_student">
+            <QRCode  value={qrCodeValue} className='qrcode_student' />
+          </div> 
+
       
-      <div className="id-card-body">
-        <strong>Birthdate:</strong>  
-        {Student.birthDate.split("-").reverse().join("-")} 
       </div>
-      
-      <div className="id-card-body">
-        <strong>Mobile Number:</strong> 
-        {Student.mobileNumber}
-      </div>
-      <div className="id-card-body">
-        <strong>Gender:</strong> {Student.gender}
-      </div>
-    
-      <div className="id-card-body">
-        <strong>Join Date:</strong> 
-        {Student.joinDate.split("-").reverse().join("-")}   
-      </div> */}
-
-<div>
-<img id="id-card-photo_student" src={userPhoto} alt="" />
-</div>
-
-        {/* <div className="id-card-name">
-        <strong> </strong>
-
-        </div>  */}
-
-        <div className="id-card-name_student">
-        <h2 className='id-card-header_student'>{Student.name}</h2> 
-
-        
-
-        </div> 
-
-      <div className="id-card-body_student">
-        <QRCode  value={qrCodeValue} className='qrcode_student' />
-      </div> 
-
-    </div>
-    </StudentSidebar>
-        </>
+      </StudentSidebar>
+    </>
   );
 };
 
